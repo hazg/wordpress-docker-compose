fe8281eff31560a538caf4d2aed011923ea03c2c
hazg <hazg@mail.ru>
2022-01-17 15:54:05 +0300

    clean up, bbpress

---
+++ wp-app/migrate.php
@@ -0,0 +1,15 @@
+<?php
+
+  // Load the WordPress library.
+  require_once __DIR__ . '/wp-load.php';
+
+  // Set up the WordPress query.
+  wp();
+
+  print 'test';
+
+  function fff(){
+
+  }
+
+?>
